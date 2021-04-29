1e657214b22508abf9c34254e2e84b5d979b42fb
DriesKerkhofs <driesk7@gmail.com>
2021-04-29 10:59:07 +0200

    bugfix

--- RTL/cpu.v
+++ RTL/cpu.v
@@ -415,17 +415,17 @@ module forwarding_unit(
 
    always@(*)begin
       if(Rs == MEM_Rd && MEM_Rd != 5'b00000 && MEM_reg_write == 1'b1)           // EX hazard
-        forward_A = 2'b11;
+        forward_Rs = 2'b11;
       else if(Rs == WB_Rd && WB_Rd != 5'b00000 && WB_reg_write == 1'b1)         // MEM hazard
-        forward_A = 2'b10;
+        forward_Rs = 2'b10;
       else
-        forward_A = 2'b00;
+        forward_Rs = 2'b00;
 
       if(Rt == MEM_Rd && MEM_Rd != 5'b00000 && MEM_reg_write == 1'b1)           // EX hazard
-        forward_B = 2'b11;
+        forward_Rt = 2'b11;
       else if(Rt == WB_Rd && WB_Rd != 5'b00000 && WB_reg_write == 1'b1)    // MEM hazard
-        forward_B = 2'b10;
+        forward_Rt = 2'b10;
       else
-        forward_B = 2'b00;
+        forward_Rt = 2'b00;
    end
 endmodule
